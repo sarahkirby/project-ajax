a97a88de2fdd04a1b24e73e3bc37d0fa94c448c8
Sarah Kirby <sk.irby@icloud.com>
2016-01-22 15:23:23 +1300

    Use ajax to connect to yoobee.net

--- js/ajax-yoobee.js
+++ js/ajax-yoobee.js
@@ -1,1 +1,34 @@
-alert('hi');
+$(document).ready(function(){
+
+	// Listen to the form. e - event
+	$('#note-form').submit( function( e ){
+
+		// Prevent form from submitting
+		e.preventDefault();
+
+		// Grab the note
+		if ( $.trim($('#note').val() )  == ''){
+			return;
+		}
+
+		// Prepare AJAX
+
+		$.ajax({
+			url: 'http://sarah.kirby.yoobee.net.nz/project-ajax/ajax-yoobee.php',
+			data: { note: $('#note').val() },
+			success: function( dataFromServer) {
+
+			 },
+			 error: function(){
+			 	alert('Cannot connect');
+			 }
+
+
+
+		});
+
+
+
+	});
+
+});
